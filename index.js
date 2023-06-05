b5251f3aa7b1ded6479af2c7c504deeef1d28600
Waheed-Labib <labibwahid28@gmail.com>
2023-06-05 13:12:16 +0600

    server created

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
-const courses = require('./data/courses.json')
+
 
 app.get('/', (req, res) => {
     res.send('learners API running')
 })
 
-app.get('/courses', (req, res) => {
-    res.send(courses)
-})
+
 
 app.listen(port, () => {
     console.log('learners server running on port', port)
